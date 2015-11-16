5e98c1def8b7d997a0be08c610ee2096df4afff8
amittal91 <amittal@ncsu.edu>
2015-11-15 23:34:56 -0500

    Testing pmd

--- App/app.js
+++ App/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello everyone! This is a very basic app for Milestone 3');
 });
 
+var x=0;
+if (x==0)
+    x=x+1
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
