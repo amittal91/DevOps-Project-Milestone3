b984b1de73d372861bef863944340b6908dc20dd
Karishma Nimgaonkar <karunim28@gmail.com>
2015-11-16 21:21:37 -0500

    Added feature flags

--- featureFlag.js
+++ featureFlag.js
@@ -20,37 +20,32 @@ catch (err) {
 var client = redis.createClient(redis_port, redis_ip, {})
 
 app.get('/', function (req, res) {
-  res.send('Hello everyone! This is a very basic app for Milestone 3');
+  res.send('Hello everyone! This is a basic demo of Feature Flag for Milestone 3');
 });
 
-app.get('/get', function(req, res) {
-	client.get('newkey', function(err,value) { 
-		res.send(value)
-	});
-})
-
-app.get('/set', function(req, res) {
-	client.lrange('featureFlag', 0, 0, function(err, reply) {
-		if (reply == "true") {
-			client.set('newkey', 'this message will self-destruct in 10 seconds');
-			client.expire('newkey', 10);
-			res.send('set key with message successfully, key will expire in 10 seconds!')
+app.get('/feature', function(req, res) {
+	client.lpop('featureFlag', function(err, reply) {
+		if ( reply == "true") {
+			client.lpush('featureFlag', false)
+			res.send("Feature for set/get disabled !")
 		}
 		else {
-			res.send('Sorry, you cannot set a key !')
+			client.lpush('featureFlag', true)
+			res.send("Feature for set/get enabled !")
 		}
 	})
-	
 })
 
-
-// var x=0;
-// if (x==0)
-//     x=x+1
-
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
+  
+  client.del('featureFlag')
+  client.lpush('featureFlag', true)
+
+  // client.llen('featureFlag', function(err, res) {
+  // 	console.log("Length: "+ res)
+  // })
+
 });
