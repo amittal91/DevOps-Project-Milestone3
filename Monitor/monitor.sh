1c680606d0a9b819aa806561219db82caf5b3747
amittal91 <amittal@ncsu.edu>
2015-11-17 20:56:47 -0500

    Build successful

--- Monitor/monitor.sh
+++ Monitor/monitor.sh
@@ -2,15 +2,18 @@
 sysname=$(hostname)
 while :
 do
+    node redisAlert.js "no"
     cpuOutput=$(python /Monitoring/Monitor/cpuMonitor.py)
     if [ $cpuOutput -gt 0 ]; then
         echo The CPU usage is high. $cpuOutput%  | mail -s "High CPU usage for $sysname" mittal.apoorv91@gmail.com
+        node redisAlert.js "yes"
     fi
     memOutput=$(python /Monitoring/Monitor/memoryMonitor.py)
     if [ $memOutput -gt 0 ]; then
         echo The Memory usage is high. $memOutput% | mail -s "High memory usage for $sysname" mittal.apoorv91@gmail.com
+        node redisAlert.js "yes"
     fi
-    sleep 30s
+    sleep 2m
 done
 
 
